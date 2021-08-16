733a33deca65c20a60fd3ff5cb8f0f12263fbb04
Fabrício Vale <73182618+fabriciovale20@users.noreply.github.com>
2021-08-16 16:09:23 -0300

    Delete Dev Aprender directory

--- Dev Aprender/Ex01(Simulador de Dados).py
+++
@@ -1,9 +0,0 @@
-import random
-print('{:-^40}'.format('SIMULADO DE DADOS'))
-resp = str(input('Vamos começar? [S/N] ')).strip().upper()[0]
-dado = 0
-while resp not in 'Nn':
-    dado = random.randint(1, 6)
-    print(f'O número sorteado foi {dado}.')
-    resp = str(input('Vamos jogar? [S/N] ')).strip().upper()[0]
-print('{:-^40}'.format('PROGRAMA FINALIZADO'))
--- Dev Aprender/Salvando Registros.py
+++
@@ -1,5 +0,0 @@
-registros = []
-with open ('registros.txt','w') as arquivo:
-    for valor in registros:
-        arquivo.write('Nome: ')
-    arquivo.write('-'*20)
