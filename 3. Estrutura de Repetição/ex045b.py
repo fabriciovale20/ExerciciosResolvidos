e07048d74d450f290d794e7888ebc912dd73fa21
Fabrício Vale <fabriciovale18@gmail.com>
2021-10-02 22:56:51 -0300

    Exercícios Resolvidos

--- 3. Estrutura de Repetição/ex045b.py
+++ 3. Estrutura de Repetição/ex045b.py
@@ -1,5 +1,5 @@
 """
-    Exercício 45 ( Sen gabarito )
+    Exercício 45 ( Gabarito não definido )
 
     Desenvolver um programa para verificar a nota do aluno em uma prova com 10 questões, o programa deve perguntar ao
 aluno a resposta de cada questão e ao final comparar com o gabarito da prova e assim calcular o total de acertos e a
